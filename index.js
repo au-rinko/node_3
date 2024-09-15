0540d2d023b5dbb6ba5e104945006b283112a657
Iryna S. <78602594+au-rinko@users.noreply.github.com>
2024-09-15 17:10:57 +0300

    feat: get data from kinopoisk and write it to json file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const fs = require('fs');
+const { getTop250Movies, TOP_250 } = require('./fetchData');
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hi there!');
 });
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
-})
+});
